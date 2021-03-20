006c62f72d5650065707f7ef5e3d1185ac28a648
Gilles Theunissen <gilles.theunissen@ulb.be>
2021-03-20 16:02:52 +0100

    Debut minimax

--- src/ai/my_ai.py
+++ src/ai/my_ai.py
@@ -10,6 +10,9 @@ Code your AI in this file.
 
 # Define your persistent variables here
 
+depth = 1
+infinity = 10000
+
 
 def make_play(board, your_team, last_move):
     """
@@ -30,8 +33,6 @@ def make_play(board, your_team, last_move):
 
     """
 
-    depth = 1
-
     all_moves = board.get_legal_moves(your_team)
     best_move = all_moves[0]
     for i in range(depth):
@@ -39,14 +40,51 @@ def make_play(board, your_team, last_move):
         new_board = board.copy_state()
         new_board.play_command(Command(selected_move[0], selected_move[1]))
 
-        black_number, white_number = 0, 0
-        for entity in new_board.get_entities():
-            team = entity.get_team()
-            if team == Team.WHITE:
-                white_number += 1
-            else:
-                black_number += 1
+        calscore(new_board)
 
         best_move = selected_move
 
     return best_move
+
+
+def calscore(new_board):
+    score = 0
+    for entity in new_board.get_entities():
+        team = entity.get_team()
+        if team == Team.WHITE:
+            score += 1
+        else:
+            score -= 1
+
+    return score
+
+
+def endgame(board):
+    return board.search_queen(Team.WHITE) is None or board.search_queen(Team.BLACK) is None
+
+
+def minimax(board, depth, maximizingPlayer):
+    if depth == 0 or endgame(board):
+        return calscore(board)
+
+    if maximizingPlayer:
+        maxeval = -infinity
+        all_moves = board.get_legal_moves(Team.WHITE)
+        for current_move in all_moves:
+            new_board = board.copy_state()
+            new_board.play_command(Command(current_move[0], current_move[1]))
+            eval = minimax(new_board, depth - 1, False)
+            maxeval = max(maxeval, eval)
+        return maxeval
+
+    else:
+        mineval = +infinity
+        all_moves = board.get_legal_moves(Team.BLACK)
+        for current_move in all_moves:
+            new_board = board.copy_state()
+            new_board.play_command(Command(current_move[0], current_move[1]))
+            eval = minimax(new_board, depth - 1, True)
+            mineval = min(mineval, eval)
+        return mineval
+
+
